11e4b55170005a73a21b9acba328b437981dd30c
jafaransari112@gmail.com <jafaransari112@gmail.com>
2023-10-26 19:41:17 +0530

    Added Greeting Model

---
+++ GreetingManagement.Models/Greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingManagement.Models
+{
+    internal class Greeting
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime DOJ { get; set; }
+
+    }
+}
